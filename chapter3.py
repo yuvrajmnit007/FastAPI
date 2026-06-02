2846eb609cd527dfa2c8fc0ac795c6df5fc919b0
yuvrajmnit007 <yuvrajchoudhary3768@gmail.com>
2026-06-03 00:28:47 +0530

    query params added sorting on the basis of height weight bmi and age in asc and desc order

--- chapter3.py
+++ chapter3.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI ,Path,HTTPException
+from fastapi import FastAPI ,Path,HTTPException,Query
 import json 
 app=FastAPI()
 
@@ -26,3 +26,18 @@ def view_patient(patient_id:str=Path(...,description='ID of the patient in the D
     if patient_id in data :
         return data[patient_id]
     raise HTTPException(status_code=404,detail='patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by:str=Query(..., description='sort on the basis of height,weightor bmi'),order:str=Query('asc', description='sort asc and desc order')):
+        vaild_fields=['height','weight','bmi','age']
+        if sort_by not in vaild_fields:
+            raise HTTPException(status_code=400,detail='invalid field selected from {valid_fields}')
+        
+        if order not in ['asc','desc']:
+            raise HTTPException(status_code=400,detail='Invalid order select between asc and desc')
+            
+        data=load_data()
+        sort_order=True if order=='desc' else False
+
+        sorted_data=sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+        return sorted_data
